85a29d0eb72fa203fad6cdfd5441ec098b167b23
aghosh376 <arayboghosh@gmail.com>
2025-03-07 19:26:50 -0800

    Initial streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.hello("Hello World")
+
+st.header("This is a header with a divider", divider='rainbow')
+st.subheader("This is a subheader")
+
+st.markdown("This is a markdown text")
+
+x =st.slider("Select a value", 1, 10)
+st.write("The value of ***x*** is ", x)
