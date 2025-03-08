6878271477ba0a329abc66f4200f95b3f2836afa
E.K. <fake.hack376@gmail.com>
2025-03-08 04:28:49 -0800

    printing gemini response on 'page 1'

--- pages/page1.py
+++ pages/page1.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from google import genai
+#from google.genai import types
+import config
+
+API_KEY = config.api
+
+client = genai.Client(api_key= API_KEY)
+
+response = client.models.generate_content(model="gemini-2.0-flash", contents="Write hello world in python")
 
 st.write("Hello World \n\nThis is page 1 of many")
+st.write("Below is google gemini 2.0 flash's response to 'Write hello world in python'")
+st.write(response.text)
+st.write("End gemini")
