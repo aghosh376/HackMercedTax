ca1fb4e122e457dfbb27eb2315408c0bfa361cda
E.K. <fake.hack376@gmail.com>
2025-03-08 14:46:18 -0800

    gemini response with file

--- pages/page1.py
+++ pages/page1.py
@@ -1,15 +1,31 @@
 import streamlit as st
 from google import genai
-#from google.genai import types
+from google.genai import types
 import config
 
 API_KEY = config.api
 
 client = genai.Client(api_key= API_KEY)
 
-response = client.models.generate_content(model="gemini-2.0-flash", contents="Write hello world in python")
-
 st.write("Hello World \n\nThis is page 1 of many")
-st.write("Below is google gemini 2.0 flash's response to 'Write hello world in python'")
-st.write(response.text)
-st.write("End gemini")
+
+
+prompt = st.text_input("Enter a prompt for Gemini 2.0")
+fileUpload = st.file_uploader("Please input a file")
+
+if fileUpload is not None:
+    file = client.files.upload(file=fileUpload)
+
+    if st.checkbox("I want to only look at my prompt response"):
+        response1 = client.models.generate_content(model="gemini-2.0-flash", contents= prompt)
+        st.write(f"Below is google gemini 2.0 flash's response to '{prompt}'")
+        st.write(response1.text)
+        st.write("End gemini")
+
+    if st.checkbox("I want to see the response to my prompt and file"):
+        response2 = client.models.generate_content(model="gemini-2.0-flash", contents=[prompt, file])
+        st.write(f"Below is google gemini 2.0 flash's response to your prompt '{prompt}' and file upload")
+        st.write(response2.text)
+        st.write("End gemini")
+else:
+    st.write("Please upload a file to proceed.")
