85c2bea3eebb2988a05bfc3f0d9254ea0ae184a8
E.K. <fake.hack376@gmail.com>
2025-03-08 00:54:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ st.subheader("This is a subheader")
 
 st.markdown("This is a markdown text")
 
-x =st.slider("Select a value", 1, 10)
+x = st.slider("Select a value", 1, 10)
 st.write("The value of ***x*** is ", x)
