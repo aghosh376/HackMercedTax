680648496eaf8d35aca8848b8b02fd344c92e9ac
Raybo <arayboghosh@gmail.com>
2025-03-08 00:58:17 -0800

    Example code

--- app.py
+++ app.py
@@ -1,11 +1,105 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import time
 
-st.hello("Hello World")
+
+st.header("Hello World", divider='blue')
 
 st.header("This is a header with a divider", divider='rainbow')
 st.subheader("This is a subheader")
 
 st.markdown("This is a markdown text")
 
-x =st.slider("Select a value", 1, 10)
-st.write("The value of ***x*** is ", x)
+col1, col2 = st.columns(2)
+
+with col1:
+    x =st.slider("Select a value", 1, 10)
+with col2:
+    st.write("The value of :blue[***x***] is ", x)
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
+    
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+    
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+    
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
